29746aaf04ee6fe57caf2f69cc9d164720661c1e
Abduvahob <dilmurodayitboyev@gmail.com>
2022-11-18 18:27:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log (
+//    [
+//        "salom js",
+//        null ,
+//        false,
+//        "nima gap",
+// //       "js yoqdimi",
+//        "korishguncha",
+//    ]
+// )
+// console.log (
+//    [
+//        "ryzen 5" ,
+//        false ,
+//        null ,
+//        "8/512gb" ,
+//        "windows 11" ,
+//        "uchrashguncha" ,
+//    ]
+// )//
+alert(" ENG WELCOME || RUS ДОБРО ПОЖАЛОВАТЬ || UZB XUSH KELIBSIZ||  ");
+
+var username = prompt("Ismingizni kiriting");
+console.log("name:" + username);
+var surname = prompt(" familangizni kiriting");
+console.log("surname:" + surname);
+var young = prompt("yoshingizni kiriting");
+console.log("young:" + young);
+var howmuchmoney = prompt("nech pul topas");
+console.log("how much money:" + howmuchmoney);
+var wheredoyoustudy = prompt("qayerda oqiysiz");
+console.log("where do you study:" + wheredoyoustudy);
